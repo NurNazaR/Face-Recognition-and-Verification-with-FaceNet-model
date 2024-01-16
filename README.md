50d78e992eefb7f60d28123c2e7c616733bd15ff
NurNazaR <nurdauletnazarbai@gmail.com>
2024-01-16 23:43:42 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,63 @@
+
+#  Face Recognition and Verification with FaceNet model
+
+This repository provides demonstration of how to build accurate face recognition system using the state-of-the-art FaceNet model. The system is deployed in Kivy and do following:
+- Face detection: detect and localize faces in the given image using MTCNN 
+- Face verification: task that involves verifying the identity of a person with a given image. Is this the same person?
+- Face recognition: task that involves recognizing the identity of a person from a database of known faces. Who is this person?
+- One-shot-Learning: Machine learning approach where a model is trained to recognize or classify objects based on a single or very few examples per class  
+
+
+## Acknowledgements
+
+Usefull resources
+* [Florian Schroff, Dmitry Kalenichenko, James Philbin (2015). FaceNet: A Unified Embedding for Face Recognition and Clustering][1]
+* [Yaniv Taigman, Ming Yang, Marc'Aurelio Ranzato, Lior Wolf (2014). DeepFace: Closing the gap to human-level performance in face verification][2]
+* [Gregory Koch, Richard Zemel, Ruslan Salakhutdinov (2015). Siamese Neural Networks for One-shot Image Recognition][3]
+* [Jason Brownlee (2020). How to Develop a Face Recognition System Using FaceNet in Keras][4]
+
+Git hub repositories 
+* [TensorFlow to Keras][5]
+* [Official FaceNet Repository][6]
+
+[1]: https://arxiv.org/pdf/1503.03832.pdf
+[2]: https://research.fb.com/wp-content/uploads/2016/11/deepface-closing-the-gap-to-human-level-performance-in-face-verification.pdf
+[3]: https://www.cs.cmu.edu/~rsalakhu/papers/oneshot1.pdf
+[4]: https://machinelearningmastery.com/how-to-develop-a-face-recognition-system-using-facenet-in-keras-and-an-svm-classifier/
+[5]: https://github.com/nyoki-mtl/keras-facenet/blob/master/notebook/tf_to_keras.ipynb
+[6]: https://github.com/davidsandberg/facenet
+## Demo
+![fast](https://drive.google.com/file/d/116_wsb0Pj-JZsspmfVPefW_RhLgfwoxz/view?usp=drive_link)
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+
+## Documentation
+
+[Documentation](https://linktodocumentation)
+
---
+++ app/face_recognition.py
@@ -0,0 +1,370 @@
+# Import kivy dependencies first
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout
+from kivy. uix.image import Image 
+from kivy.uix.button import Button 
+from kivy.uix. label import Label 
+from kivy.clock import Clock 
+from kivy.graphics.texture import Texture 
+from kivy.logger import Logger
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.popup import Popup
+
+# Import other dependencies
+import cv2
+import tensorflow as tf 
+import os
+import numpy as np
+import mtcnn as mtcnn
+
+from pathlib import Path
+import imghdr
+import sys
+
+from functools import partial
+
+from keras.models import Model
+from keras.layers import Activation
+from keras.layers import BatchNormalization
+from keras.layers import Concatenate
+from keras.layers import Conv2D
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import Input
+from keras.layers import Lambda
+from keras.layers import MaxPooling2D
+from keras.layers import add
+from keras import backend as K
+
+
+def scaling(x, scale):
+    return x * scale
+
+
+def conv2d_bn(x,
+              filters,
+              kernel_size,
+              strides=1,
+              padding='same',
+              activation='relu',
+              use_bias=False,
+              name=None):
+    x = Conv2D(filters,
+               kernel_size,
+               strides=strides,
+               padding=padding,
+               use_bias=use_bias,
+               name=name)(x)
+    if not use_bias:
+        bn_axis = 1 if K.image_data_format() == 'channels_first' else 3
+        bn_name = _generate_layer_name('BatchNorm', prefix=name)
+        x = BatchNormalization(axis=bn_axis, momentum=0.995, epsilon=0.001,
+                               scale=False, name=bn_name)(x)
+    if activation is not None:
+        ac_name = _generate_layer_name('Activation', prefix=name)
+        x = Activation(activation, name=ac_name)(x)
+    return x
+
+
+def _generate_layer_name(name, branch_idx=None, prefix=None):
+    if prefix is None:
+        return None
+    if branch_idx is None:
+        return '_'.join((prefix, name))
+    return '_'.join((prefix, 'Branch', str(branch_idx), name))
+
+
+def _inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else 3
+    if block_idx is None:
+        prefix = None
+    else:
+        prefix = '_'.join((block_type, str(block_idx)))
+    name_fmt = partial(_generate_layer_name, prefix=prefix)
+
+    if block_type == 'Block35':
+        branch_0 = conv2d_bn(x, 32, 1, name=name_fmt('Conv2d_1x1', 0))
+        branch_1 = conv2d_bn(x, 32, 1, name=name_fmt('Conv2d_0a_1x1', 1))
+        branch_1 = conv2d_bn(branch_1, 32, 3, name=name_fmt('Conv2d_0b_3x3', 1))
+        branch_2 = conv2d_bn(x, 32, 1, name=name_fmt('Conv2d_0a_1x1', 2))
+        branch_2 = conv2d_bn(branch_2, 32, 3, name=name_fmt('Conv2d_0b_3x3', 2))
+        branch_2 = conv2d_bn(branch_2, 32, 3, name=name_fmt('Conv2d_0c_3x3', 2))
+        branches = [branch_0, branch_1, branch_2]
+    elif block_type == 'Block17':
+        branch_0 = conv2d_bn(x, 128, 1, name=name_fmt('Conv2d_1x1', 0))
+        branch_1 = conv2d_bn(x, 128, 1, name=name_fmt('Conv2d_0a_1x1', 1))
+        branch_1 = conv2d_bn(branch_1, 128, [1, 7], name=name_fmt('Conv2d_0b_1x7', 1))
+        branch_1 = conv2d_bn(branch_1, 128, [7, 1], name=name_fmt('Conv2d_0c_7x1', 1))
+        branches = [branch_0, branch_1]
+    elif block_type == 'Block8':
+        branch_0 = conv2d_bn(x, 192, 1, name=name_fmt('Conv2d_1x1', 0))
+        branch_1 = conv2d_bn(x, 192, 1, name=name_fmt('Conv2d_0a_1x1', 1))
+        branch_1 = conv2d_bn(branch_1, 192, [1, 3], name=name_fmt('Conv2d_0b_1x3', 1))
+        branch_1 = conv2d_bn(branch_1, 192, [3, 1], name=name_fmt('Conv2d_0c_3x1', 1))
+        branches = [branch_0, branch_1]
+    else:
+        raise ValueError('Unknown Inception-ResNet block type. '
+                         'Expects "Block35", "Block17" or "Block8", '
+                         'but got: ' + str(block_type))
+
+    mixed = Concatenate(axis=channel_axis, name=name_fmt('Concatenate'))(branches)
+    up = conv2d_bn(mixed,
+                   K.int_shape(x)[channel_axis],
+                   1,
+                   activation=None,
+                   use_bias=True,
+                   name=name_fmt('Conv2d_1x1'))
+    up = Lambda(scaling,
+                output_shape=K.int_shape(up)[1:],
+                arguments={'scale': scale})(up)
+    x = add([x, up])
+    if activation is not None:
+        x = Activation(activation, name=name_fmt('Activation'))(x)
+    return x
+
+
+def InceptionResNetV1(input_shape=(160, 160, 3),
+                      classes=128,
+                      dropout_keep_prob=0.8,
+                      weights_path=None):
+    inputs = Input(shape=input_shape)
+    x = conv2d_bn(inputs, 32, 3, strides=2, padding='valid', name='Conv2d_1a_3x3')
+    x = conv2d_bn(x, 32, 3, padding='valid', name='Conv2d_2a_3x3')
+    x = conv2d_bn(x, 64, 3, name='Conv2d_2b_3x3')
+    x = MaxPooling2D(3, strides=2, name='MaxPool_3a_3x3')(x)
+    x = conv2d_bn(x, 80, 1, padding='valid', name='Conv2d_3b_1x1')
+    x = conv2d_bn(x, 192, 3, padding='valid', name='Conv2d_4a_3x3')
+    x = conv2d_bn(x, 256, 3, strides=2, padding='valid', name='Conv2d_4b_3x3')
+
+    # 5x Block35 (Inception-ResNet-A block):
+    for block_idx in range(1, 6):
+        x = _inception_resnet_block(x,
+                                    scale=0.17,
+                                    block_type='Block35',
+                                    block_idx=block_idx)
+
+    # Mixed 6a (Reduction-A block):
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else 3
+    name_fmt = partial(_generate_layer_name, prefix='Mixed_6a')
+    branch_0 = conv2d_bn(x,
+                         384,
+                         3,
+                         strides=2,
+                         padding='valid',
+                         name=name_fmt('Conv2d_1a_3x3', 0))
+    branch_1 = conv2d_bn(x, 192, 1, name=name_fmt('Conv2d_0a_1x1', 1))
+    branch_1 = conv2d_bn(branch_1, 192, 3, name=name_fmt('Conv2d_0b_3x3', 1))
+    branch_1 = conv2d_bn(branch_1,
+                         256,
+                         3,
+                         strides=2,
+                         padding='valid',
+                         name=name_fmt('Conv2d_1a_3x3', 1))
+    branch_pool = MaxPooling2D(3,
+                               strides=2,
+                               padding='valid',
+                               name=name_fmt('MaxPool_1a_3x3', 2))(x)
+    branches = [branch_0, branch_1, branch_pool]
+    x = Concatenate(axis=channel_axis, name='Mixed_6a')(branches)
+
+    # 10x Block17 (Inception-ResNet-B block):
+    for block_idx in range(1, 11):
+        x = _inception_resnet_block(x,
+                                    scale=0.1,
+                                    block_type='Block17',
+                                    block_idx=block_idx)
+
+    # Mixed 7a (Reduction-B block): 8 x 8 x 2080
+    name_fmt = partial(_generate_layer_name, prefix='Mixed_7a')
+    branch_0 = conv2d_bn(x, 256, 1, name=name_fmt('Conv2d_0a_1x1', 0))
+    branch_0 = conv2d_bn(branch_0,
+                         384,
+                         3,
+                         strides=2,
+                         padding='valid',
+                         name=name_fmt('Conv2d_1a_3x3', 0))
+    branch_1 = conv2d_bn(x, 256, 1, name=name_fmt('Conv2d_0a_1x1', 1))
+    branch_1 = conv2d_bn(branch_1,
+                         256,
+                         3,
+                         strides=2,
+                         padding='valid',
+                         name=name_fmt('Conv2d_1a_3x3', 1))
+    branch_2 = conv2d_bn(x, 256, 1, name=name_fmt('Conv2d_0a_1x1', 2))
+    branch_2 = conv2d_bn(branch_2, 256, 3, name=name_fmt('Conv2d_0b_3x3', 2))
+    branch_2 = conv2d_bn(branch_2,
+                         256,
+                         3,
+                         strides=2,
+                         padding='valid',
+                         name=name_fmt('Conv2d_1a_3x3', 2))
+    branch_pool = MaxPooling2D(3,
+                               strides=2,
+                               padding='valid',
+                               name=name_fmt('MaxPool_1a_3x3', 3))(x)
+    branches = [branch_0, branch_1, branch_2, branch_pool]
+    x = Concatenate(axis=channel_axis, name='Mixed_7a')(branches)
+
+    # 5x Block8 (Inception-ResNet-C block):
+    for block_idx in range(1, 6):
+        x = _inception_resnet_block(x,
+                                    scale=0.2,
+                                    block_type='Block8',
+                                    block_idx=block_idx)
+    x = _inception_resnet_block(x,
+                                scale=1.,
+                                activation=None,
+                                block_type='Block8',
+                                block_idx=6)
+
+    # Classification block
+    x = GlobalAveragePooling2D(name='AvgPool')(x)
+    x = Dropout(1.0 - dropout_keep_prob, name='Dropout')(x)
+    # Bottleneck
+    x = Dense(classes, use_bias=False, name='Bottleneck')(x)
+    bn_name = _generate_layer_name('BatchNorm', prefix='Bottleneck')
+    x = BatchNormalization(momentum=0.995, epsilon=0.001, scale=False,
+                           name=bn_name)(x)
+
+    # Create model
+    model = Model(inputs, x, name='inception_resnet_v1')
+    if weights_path is not None:
+        model.load_weights(weights_path)
+
+    return model
+
+path = os.path.join(os.getcwd(), "keras-facenet", "weights", "facenet_keras_weights.h5")
+
+model = InceptionResNetV1(weights_path = path)
+
+# Build app and layout
+class CamApp(App):
+    def build(self):
+        # Main lavout components
+        self.web_cam = Image(size_hint=(1, .8))
+        self.button = Button (text="Verify", on_press=self.who_is_it, size_hint=(1, .1))
+        self.verification_label = Label(text ="Verification Uninitiated", size_hint= (1, .1))
+        self.upload_button = Button(text='Upload Photo to Database', size_hint=(1, .1))
+        self.upload_button.bind(on_release=self.show_file_chooser)
+        
+        # Add items to layout
+        layout = BoxLayout(orientation= 'vertical')
+        layout.add_widget(self.web_cam)
+        layout.add_widget(self.button)
+        layout.add_widget(self.verification_label)
+        layout.add_widget(self.upload_button)
+        # Load tensorflow/keras model
+        self.model = model # tf.keras.models.load_model('model.keras', safe_mode = False)
+        
+        # Buidling database
+        self.database = {}
+        for image in os.listdir(os.path.join(os.getcwd(), "database (people)")):
+            if image != ".DS_Store":
+                person_name = image.split(".")[0]
+                if person_name not in self.database:
+                    image_path = os.path.join("database (people)", image)
+                    self.database[person_name] = self.img_to_encoding(image_path, self.model)
+                
+        # Setup video capture device
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0/33.0) 
+        
+        return layout
+    
+    # Run continuously to get webcam feed
+    def update(self, *args):
+        # Read frame from openc
+        ret, frame = self.capture.read()
+        
+        frame = cv2.flip(frame, 1)
+        
+        # Flip horizontall and convert image to texture
+        buf = cv2.flip (frame, 0).tobytes()
+        img_texture = Texture.create(size= (frame. shape [1], frame. shape [0]), colorfmt='bgr')
+        img_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        self.web_cam.texture = img_texture
+    
+    
+    def show_file_chooser(self, instance):
+        file_chooser = FileChooserListView(path='~')
+        file_chooser.bind(on_submit=self.upload_photo)
+        self.popup = Popup(title='Choose a Photo', content=file_chooser, size_hint=(0.8, 0.8))
+        self.popup.open()
+        
+        
+    def upload_photo(self, instance, path, filename):
+        # Here, you can write the code to upload the photo to the database
+        print(f"Uploading photo: {filename} from {path} to the database")
+        
+        # Close the popup after the photo is uploaded
+        self.popup.dismiss()
+    
+    def extract_face(self, image_path, required_size=(160, 160)):
+        # load image from file
+        image = cv2.imread(image_path)
+        # converting BGR to RGB 
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # create the detector, using default weights
+        detector = mtcnn.MTCNN()
+        # detect faces in the image
+        results = detector.detect_faces(image_rgb)
+        
+        # extract the bounding box from the first face
+        x1, y1, width, height = results[0]['box']
+        # bug fix
+        x1, y1 = abs(x1), abs(y1)
+        x2, y2 = x1 + width, y1 + height
+        
+        # extract the face
+        face = image_rgb[y1:y2, x1:x2, :]
+        
+        return face
+    
+    def img_to_encoding(self, image_path, model):
+        face = self.extract_face(image_path, required_size=(160, 160))
+        img = tf.image.resize(face, (160, 160))
+        img = np.around(np.array(img) / 255.0, decimals=12)
+        x_train = np.expand_dims(img, axis=0)
+        embedding = model.predict_on_batch(x_train)
+        return embedding / np.linalg.norm(embedding, ord=2)
+    
+    def who_is_it(self, image_path):
+        
+        # Capture input image from our webcam
+        SAVE_PATH = os.path.join('input_image.jpg')
+        ret, frame = self.capture.read()
+        cv2.imwrite(SAVE_PATH, frame)
+        
+        encoding = self.img_to_encoding(SAVE_PATH, self.model)
+        
+        min_dist = 100
+    
+        # Loop over the database dictionary's names and encodings.
+        for (name, db_enc) in self.database.items():
+            
+            # Compute L2 distance between the target "encoding" and the current db_enc from the database. (≈ 1 line)
+            dist = np.linalg.norm(encoding - db_enc)
+
+            # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. (≈ 3 lines)
+            if dist < min_dist:
+                min_dist = dist
+                identity = name
+                
+    
+        if min_dist < 0.7:
+            self.verification_label.text = "It's " + str(identity) + ", welcome in!"
+            door_open = True 
+        else:
+            self.verification_label.text = "Not in the database. " + str(min_dist)
+            door_open = False
+        
+        # Log out details
+        Logger.info(dist)
+        Logger.info(door_open)
+        return dist, door_open
+    
+         
+if __name__ == '__main__':
+    CamApp().run()
